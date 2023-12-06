9f9fd6bd7c7420b01a86adfbd98c5ee2e8437cf0
Lindorm236 <elielbjean@gmail.com>
2023-12-06 14:32:37 +0000

    Création du premier formulaire

---
+++ signup.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form>
+        <label for="user_name">Nom d'utilisateur</label>
+        <input type="text" name="user_name" id="user_name">
+    </form>
+</body>
+
+</html>
