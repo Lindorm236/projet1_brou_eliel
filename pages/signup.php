007db7eaedda6903efda96f89b2d4b4c6b205ea1
Lindorm236 <elielbjean@gmail.com>
2023-12-06 14:50:38 +0000

    validations et liaison vers la page de login

---
+++ pages/signup.php
@@ -0,0 +1,63 @@
+<?php
+session_start();
+var_dump($_SESSION);
+$user_name = '';
+if (isset($_SESSION['signup_form']['user_name'])) {
+    $user_name = $_SESSION['signup_form']['user_name'];
+}
+$email = '';
+if (isset($_SESSION['signup_form']['email'])) {
+    $email = $_SESSION['signup_form']['email'];
+}
+$pwd = '';
+if (isset($_SESSION['signup_form']['pwd'])) {
+    $pwd = $_SESSION['signup_form']['pwd'];
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styleIndex.css">
+</head>
+
+<body>
+    <!-- <nav class="menu">
+        <a href="signupResult.php">accueil</a>
+    </nav> -->
+    <form action="../results/signupResult.php" method="post" name="formInscription">
+        <label for="user_name">Nom d'utilisateur</label>
+        <!-- Validation du user name -->
+        <input type="text" name="user_name" id="user_name" value="<?php echo isset($_SESSION['signup_form']['user_name']) ? $_SESSION['signup_form']['user_name'] : '' ?>">
+
+        <p style="color: red; font-size: 0.8rem;"><?php echo isset($_SESSION['signup_errors']['user_name']) ? $_SESSION['signup_errors']['user_name'] : '' ?></p>
+
+
+        <!-- Validation du user name -->
+        <label for="email">Email</label>
+        <input type="text" name="email" id="email" value="<?php echo isset($_SESSION['signup_form']['email']) ? $_SESSION['signup_form']['email'] : '' ?>">
+        <p style="color: red; font-size: 0.8rem;"><?php echo isset($_SESSION['signup_errors']['email']) ? $_SESSION['signup_errors']['email'] : '' ?></p>
+
+        <label for="pwd">Mot de passe</label>
+        <input type="password" name="pwd" id="pwd" value="<?php echo isset($_SESSION['signup_form']['pwd']) ? $_SESSION['signup_form']['pwd'] : '' ?>">
+        <p style="color: red; font-size: 0.8rem;"><?php echo isset($_SESSION['signup_errors']['pwd']) ? $_SESSION['signup_errors']['pwd'] : '' ?></p>
+
+        <label for="fname">Nom</label>
+        <input type="text" name="fname" id="fname" value="<?php echo isset($_SESSION['signup_form']['fname']) ? $_SESSION['signup_form']['fname'] : '' ?>">
+        <p style="color: red; font-size: 0.8rem;"><?php echo isset($_SESSION['signup_errors']['fname']) ? $_SESSION['signup_errors']['fname'] : '' ?></p>
+
+        <label for="lname">Prenom(s)</label>
+        <input type="text" name="lname" id="lname" value="<?php echo isset($_SESSION['signup_form']['lname']) ? $_SESSION['signup_form']['lname'] : '' ?>">
+        <p style="color: red; font-size: 0.8rem;"><?php echo isset($_SESSION['signup_errors']['lname']) ? $_SESSION['signup_errors']['lname'] : '' ?></p>
+
+        <button type="submit" name="formInscription">S'inscrire</button>
+    </form>
+</body>
+<?php
+
+?>
+
+</html>
---
+++ results/signupResult.php
@@ -0,0 +1,124 @@
+<?php
+require_once "../functions/userCrud.php";
+require_once "../functions/validationSignup.php";
+session_start();
+/*if (isset($_POST["formInscription"]) && !empty($_POST["formInscription"])) {
+    echo "<input type='text'";
+}*/
+//Verification des champs()
+if (isset($_POST["formInscription"])) {
+    // echo ("coucou vous n'avez pas rempli le formulaire");
+    var_dump($_POST);
+    $_SESSION['signup_form'] = $_POST;
+
+    unset($_SESSION['signup_errors']);
+
+
+    //Verification user name
+    if (isset($_POST["user_name"]) and isset($_POST["email"]) and isset($_POST["pwd"]) and isset($_POST["fname"]) and isset($_POST["lname"])) {
+        // echo "<p>error : </p>";
+
+        // vider les messages d'erreur
+
+        $userNameValidationData = is_user_name_valid($_POST["user_name"]);
+
+        $emailValidationData = is_email_valid($_POST["email"]);
+        $passwordValidationData = is_password_valid($_POST["pwd"]);
+        $fnameValidationData = is_fname_valid($_POST["fname"]);
+        $lnameValidationData = is_lname_valid($_POST["lname"]);
+
+        $verif = true;
+        //Verification du user name
+        if (!$userNameValidationData["isValid"]) {
+            echo "<input type='text'>";
+            echo "<br>";
+            echo ($userNameValidationData["msg"]);
+            echo "<br>";
+            echo "<br>";
+            $verif = false;
+        }
+
+        //verification de l'email
+        if (!$emailValidationData["isValid"]) {
+            echo "<input type='text'>";
+            echo "<br>";
+            echo ($emailValidationData["msg"]);
+            echo "<br>";
+            echo "<br>";
+            $verif = false;
+        }
+
+        //verification du password
+        if (!$passwordValidationData["isValid"]) {
+?>
+            <input type="text" value="<?php $_POST["user_name"]; ?>">
+<?php
+            echo "<br>";
+            echo ($passwordValidationData["msg"]);
+
+            echo "<br>";
+            echo "<br>";
+            $verif = false;
+        }
+
+        //verification du nom
+        if (!$fnameValidationData["isValid"]) {
+            echo "<input type='text'>";
+            echo "<br>";
+            echo ($fnameValidationData["msg"]);
+            echo "<br>";
+            echo "<br>";
+            $verif = false;
+        }
+
+        //verification du prénom
+        if (!$lnameValidationData["isValid"]) {
+            echo "<input type='text'>";
+            echo "<br>";
+            echo ($lnameValidationData["msg"]);
+            echo "<br>";
+            echo "<br>";
+            $verif = false;
+        }
+
+        if ($verif) {
+            // si verif == true : on veut  enregistrer dans la DB puis aller a la page login
+            echo ("super");
+            $token = hash('sha256', random_bytes(32));
+            $data = [
+                'user_name' => $_POST['user_name'],
+                'email' => $_POST['email'],
+                'pwd' => $_POST["pwd"],
+                'fname' => $_POST['fname'],
+                'lname' => $_POST['lname'],
+                'billing_address_id' => 0,
+                'shipping_adress_id' => 0,
+                'token' => $token,
+                'role_id' => 3
+            ];
+            var_dump($data);
+            //enregistrer dans la DB
+            insertInstanceIntoTable($data['user_name'], $data['email'], $data['pwd'], $data['fname'], $data['lname'], $data['billing_address_id'], $data['shipping_address_id'], $data['token'], $data['role_id']);
+            // redirect vers login
+            $url = '../pages/login.php';
+            header('Location: ' . $url);
+        } else {
+            // si verif == false : on veut revenir sur le for, et qfficher nos ,essqges d.erreur
+            echo ("faux");
+            $_SESSION['signup_errors'] = [
+                'user_name' => $userNameValidationData['msg'],
+                'email' => $emailValidationData['msg'],
+                'pwd' => $passwordValidationData['msg'],
+                'fname' => $fnameValidationData['msg'],
+                'lname' => $lnameValidationData['msg']
+            ];
+            $url = '../pages/signup.php';
+            header('Location: ' . $url);
+            //recuperer les messqges d ereur dans un array 
+            // rendre se array dispo pour les afficher dans le form
+            // revenir sur le form
+            // afficher les messages d.erreur
+
+        }
+    }
+}
