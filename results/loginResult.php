64670e9edcd80bff351a109c1016b39cd33363d3
Lindorm236 <elielbjean@gmail.com>
2023-12-08 14:04:20 +0000

    validations du login

---
+++ results/loginResult.php
@@ -0,0 +1,88 @@
+<?php
+session_start();
+// {todo : si les informations entrées dans 
+//la base de données, 
+//faire une direction vers une page en fonction de son statut}
+
+//Sinon 
+?>
+<a href="../">Accueil</a>
+<h2>Login result</h2>
+<?php
+
+require_once '../functions/userCrud.php';
+require_once "../functions/functions.php";
+require_once '../utils/connexion.php';
+
+
+// var_dump($_POST);
+
+// if (isset($_POST)) {
+
+//     //vérifier si username dans DB
+//     if (!empty($_POST['user_name'])) {
+//         $userData = getUserByUsername($_POST['user_name']);
+//         if ($userData) {
+//             $url = '../pages/signup.php';
+//             header('Location: ' . $url);
+//             // $result = [
+//             //     "isValid" => false,
+//             //     "msg" => "existe deja"
+//             // ];
+//         } else {
+//             //Erreur rien entré
+//             //redirect vers login
+//             $url = '../pages/login.php';
+//             header('Location: ' . $url);
+//         }
+//     }
+// }
+
+
+
+if (isset($_POST)) {
+
+    //vérifier si username dans DB
+    if (!empty($_POST['user_name'])) {
+        var_dump($_POST['user_name']);
+
+        $userData = getUserByUsername($_POST['user_name']);
+    } else {
+        //Erreur rien entré
+        //redirect vers login
+        $url = '../pages/login.php';
+        header('Location: ' . $url);
+    }
+
+
+    //si l'utilisateur exist dans la DB
+    if ($userData) {
+
+        // comparer pwd avec DB (version encodée)
+        $enteredPwdEncoded = encodePwd($_POST['pwd']);
+        if ($userData['pwd'] == $enteredPwdEncoded) {
+            //traitement si mdp correct
+            //créeer un token
+            $token = hash('sha256', random_bytes(32));
+            echo '</br></br>Mon token : </br>';
+
+            var_dump($token);
+            //enregistrer le token en Session et dans la DB
+
+            echo "C'est le bon mdp ";
+        } else {
+            //traitement si mdp incorrect
+            //compter lenombre d'erreur et bloquer l'IP apres 3 erreur
+            //Les erreurs peuvent etre dans une Session
+            //Proposer de réinitialiser le mdp
+            //Créer un msg d'erreur
+            //renvoyer sur la page login
+            echo "C'est pas le bon mdp ";
+        }
+    }
+} else {
+    //redirect vers login
+    echo ("non");
+    $url = '../pages/login.php';
+    header('Location: ' . $url);
+}
