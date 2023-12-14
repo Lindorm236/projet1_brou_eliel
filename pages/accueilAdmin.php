4a40b2bb588274cdf560daf653e5903d8217702a
Lindorm236 <elielbjean@gmail.com>
2023-12-14 18:25:39 +0000

    ajout de bootstrap

--- pages/accueilAdmin.php
+++ pages/accueilAdmin.php
@@ -4,11 +4,94 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/styleAccueilAdmin.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 
 <body>
-    <p>Bienvenue</p>
+    <form>
+        <html lang="en" data-bs-theme="auto">
+
+        <head>
+            <script src="/docs/5.3/assets/js/color-modes.js"></script>
+
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <meta name="description" content="">
+            <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+            <meta name="generator" content="Hugo 0.118.2">
+            <title>Headers · Bootstrap v5.3</title>
+
+            <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/headers/">
+
+
+
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
+
+            <link href="/docs/5.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+            <!-- Favicons -->
+            <link rel="apple-touch-icon" href="/docs/5.3/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+            <link rel="icon" href="/docs/5.3/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+            <link rel="icon" href="/docs/5.3/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+            <link rel="manifest" href="/docs/5.3/assets/img/favicons/manifest.json">
+            <link rel="mask-icon" href="/docs/5.3/assets/img/favicons/safari-pinned-tab.svg" color="#712cf9">
+            <link rel="icon" href="/docs/5.3/assets/img/favicons/favicon.ico">
+            <meta name="theme-color" content="#712cf9">
+
+
+            <style>
+
+            </style>
+
+
+            <!-- Custom styles for this template -->
+            <link href="headers.css" rel="stylesheet">
+        </head>
+
+        <body>
+
+            <!-- <div class="b-example-divider"></div> -->
+
+            <header class="p-3 text-bg-dark">
+                <div class="container">
+                    <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                            <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
+                                <use xlink:href="#bootstrap" />
+                            </svg>
+                        </a>
+
+                        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                            <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
+                            <li><a href="#" class="nav-link px-2 text-white">Gérer les utilisateurs</a></li>
+                            <li><a href="#" class="nav-link px-2 text-white">Ajouter des produits</a></li>
+                            <li><a href="#" class="nav-link px-2 text-white">Supprimer des produits</a></li>
+                            <li><a href="../images/AboutUs.html" class="nav-link px-2 text-white">AboutUs</a></li>
+                        </ul>
+
+                        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+                            <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search">
+                        </form>
+
+                        <div class="text-end">
+                            <button type="button" class="btn btn-outline-light me-2">Login</button>
+                            <button type="button" class="btn btn-warning">Sign-up</button>
+                        </div>
+                    </div>
+                </div>
+            </header>
+
+            <div class="b-example-divider"></div>
+
+
+        </body>
+
+        </html>
+    </form>
+
 </body>
 
 </html>
